ba3b9cd5ec5169e69641f83ff0908d9bb3af113f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-19 14:20:53 -0700

    simplify producer config

--- producer/src/producer.py
+++ producer/src/producer.py
@@ -3,15 +3,15 @@ import time
 from confluent_kafka import Producer
 from stock_price import random_price_change
 
-topic = os.environ.get('PRODUCER_TOPIC', 'stock_price_changes')
-delay = float(os.environ.get("PRODUCER_DELAY", "1"))
+delay = float(os.environ.get("PRODUCER_SPEED_MS", "1000")) / 1000.0
 
-producer = Producer({"bootstrap.servers": "kafka:9092"})
+producer = Producer({"bootstrap.servers": 'kafka:9092'})
 
+# continually stream stock price changes to kafka
 def stream():
     while True:
         update = random_price_change()
-        producer.produce(topic, key=update[0], value=str(update[1]))
+        producer.produce("stock_price_changes", key=update[0], value=str(update[1]))
         time.sleep(delay)
 
 stream()
