878985fa18990f484731d805691db57ce03944ce
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-19 14:12:49 -0700

    produce messages to kafka

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kafka-hello-world
 Simple Kafka streaming
+
+## Run 
+```
+docker compose up -d
+```
+
+## Console Consumer
+```
+docker exec -it kafka  kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic stock_price_changes --from-beginning
+
+```
--- producer/src/producer.py
+++ producer/src/producer.py
@@ -3,11 +3,15 @@ import time
 from confluent_kafka import Producer
 from stock_price import random_price_change
 
+topic = os.environ.get('PRODUCER_TOPIC', 'stock_price_changes')
 delay = float(os.environ.get("PRODUCER_DELAY", "1"))
+
+producer = Producer({"bootstrap.servers": "kafka:9092"})
 
 def stream():
     while True:
-        print(random_price_change())
+        update = random_price_change()
+        producer.produce(topic, key=update[0], value=str(update[1]))
         time.sleep(delay)
 
 stream()
