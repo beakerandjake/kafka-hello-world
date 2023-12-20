cd0688b22026827fbe51ec9afe6be9708582dbbb
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-20 15:03:08 -0700

    create stocks table

--- database/create.sql
+++ database/create.sql
@@ -3,3 +3,10 @@ CREATE TABLE price_changes (
     price DECIMAL(12,2) NOT NULL,
     change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE stocks (
+    id SERIAL PRIMARY KEY,
+    ticker TEXT NOT NULL,
+    price DECIMAL(12,2) NOT NULL,
+    volatility DECIMAL(3, 2)
+);
