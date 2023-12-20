ec3a5ce3b0d7b7199eb416df22580d3d45541770
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-20 14:43:38 -0700

    simplify

--- consumer/src/save_price_event.py
+++ consumer/src/save_price_event.py
@@ -1,24 +1,10 @@
 from psycopg2 import connect
 
-# wrapper around a psycopg2 connection
-class DbConnection(object):
-    def __init__(self):
-        self._connection = connect('')
-        self._connection.autocommit = True
-        self._cursor = self._connection.cursor()
-    
-    def query(self, query, params = None):
-        return self._cursor.execute(query, params)
-
-    def __del__(self):
-        self._cursor.close()
-        self._connection.close()
-
-
-connection = DbConnection()
-
+connection = connect('')
+connection.autocommit = True
+cursor = connection.cursor()
 
 # save the price event to the database
 def save_price_event(event):
     print('saving price change: {}'.format(event))
-    connection.query("INSERT INTO price_changes (ticker, price) VALUES (%s, %s)", tuple(event))
+    cursor.execute("INSERT INTO price_changes (ticker, price) VALUES (%s, %s)", tuple(event))
