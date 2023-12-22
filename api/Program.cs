e09b8c496cd60561e4fc0433bb302638b3c973c5
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-22 12:16:07 -0700

    update routing

--- api/Program.cs
+++ api/Program.cs
@@ -4,6 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<StockDb>();
 var app = builder.Build();
 
-app.MapGet("/", async (StockDb db) => await db.Stocks.ToListAsync());
+app.MapGet("/stocks", async (StockDb db) => await db.Stocks.ToListAsync());
 
 app.Run();
