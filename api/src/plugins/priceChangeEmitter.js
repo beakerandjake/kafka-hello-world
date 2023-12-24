10627c23e1fffe75aadd4ff8492aabd4a7981bc5
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-24 14:00:26 -0700

    testing out node-rdkafka

--- api/src/plugins/priceChangeEmitter.js
+++ api/src/plugins/priceChangeEmitter.js
@@ -1,35 +1,49 @@
-import { EventEmitter } from "node:events";
+import crypto from "node:crypto";
 import fp from "fastify-plugin";
-import { Kafka } from "kafkajs";
+import kafka from "node-rdkafka";
+import Emittery from "emittery";
+
+const createReadStream = ({ broker, topics }) => {
+  const globalConfig = {
+    "group.id": crypto.randomBytes(20).toString("hex"),
+    "metadata.broker.list": broker,
+  };
+  const streamOptions = { topics };
+  return kafka.KafkaConsumer.createReadStream(globalConfig, {}, streamOptions);
+};
 
 /**
  * Decorates fastify with an EventEmitter 'priceChangeEmitter'.
  * This emitter emits 'price_change' events which come from kafka.
  */
 const plugin = async (fastify, options) => {
-  const emitter = new EventEmitter();
-  const kafka = new Kafka({ clientId: "api", brokers: ["kafka:9092"] });
-  const consumer = kafka.consumer({ groupId: "api-consumer" });
-  await consumer.connect();
-  await consumer.subscribe({
-    topic: "stock_price_changes",
-    fromBeginning: false,
+  const emitter = new Emittery();
+  const stream = createReadStream(options);
+
+  // emit price change events on each kafka message received
+  stream.on("data", (message) => {
+    const parsed = JSON.parse(message.value.toString());
+    emitter.emit("price_change", parsed);
   });
 
-  consumer.run({
-    eachMessage: ({ message }) => {
-      const parsed = JSON.parse(message.value.toString());
-      emitter.emit("price_change", parsed);
-    },
-  });
-
+  // listen to server shutdown events and clean up.
   fastify.addHook("onClose", async () => {
     fastify.log.info("cleaning up price change emitter");
-    emitter.removeAllListeners();
-    await consumer.disconnect();
+    emitter.clearListeners();
+    await new Promise((resolve) => {
+      stream.close(resolve);
+    });
   });
 
   fastify.decorate("priceChangeEmitter", emitter);
 };
 
 export default fp(plugin, { fastify: "4.x" });
+
+/**
+ * Plugin configuration for fastify-autoload
+ */
+export const autoConfig = {
+  broker: process.env.API_KAFKA_BROKER,
+  topics: process.env.API_KAFKA_TOPIC,
+};
