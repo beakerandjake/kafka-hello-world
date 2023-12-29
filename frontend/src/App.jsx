fdf36d0456f6069e8e1b31ede4b7affd31480b03
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-29 15:58:03 -0700

    testing stock card scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,59 @@
 import { PageHeading } from "./components/PageHeading";
 import { StockCard } from "./components/StockCard";
 
+const stocks = [
+  {
+    ticker: "FUN",
+    price: 666.69,
+    changePercent: -1.32,
+  },
+  {
+    ticker: "YUM",
+    price: 212.48,
+    changePercent: 6.69,
+  },
+  {
+    ticker: "FUN",
+    price: 666.69,
+    changePercent: -1.32,
+  },
+  {
+    ticker: "YUM",
+    price: 212.48,
+    changePercent: 6.69,
+  },
+  {
+    ticker: "FUN",
+    price: 666.69,
+    changePercent: -1.32,
+  },
+  {
+    ticker: "YUM",
+    price: 212.48,
+    changePercent: 6.69,
+  },
+  {
+    ticker: "FUN",
+    price: 666.69,
+    changePercent: -1.32,
+  },
+  {
+    ticker: "YUM",
+    price: 212.48,
+    changePercent: 6.69,
+  },
+];
+
 function App() {
   return (
     <div className="min-h-full">
       <PageHeading />
       <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="w-full overflow-hidden flex items-center">
-            <StockCard ticker="FUN" price="666.69" />
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="flex w-full items-center gap-3 overflow-hidden overflow-x-auto pb-6">
+            {stocks.map((stock) => (
+              <StockCard key={stock.ticker} {...stock} />
+            ))}
           </div>
         </div>
       </main>
