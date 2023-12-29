e660e7cc33e0fcf9e88551870335299c89f39310
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-29 13:39:37 -0700

    fix typo

--- frontend/src/components/PageHeading.jsx
+++ frontend/src/components/PageHeading.jsx
@@ -7,7 +7,7 @@ export const PageHeading = () => (
       <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
         Kafka Hello World <span className="pl-1">📈</span>
       </h1>
-      <h2 className="dark:text-sate-400 text-md mt-2  hidden text-gray-500 md:block">
+      <h2 className="dark:text-slate-400 text-md mt-2  hidden text-gray-500 md:block">
         Real time (fake) stock prices with Kafka, Postgres, Fastify and React
       </h2>
     </div>
