a2d80d5f56604277355c4659c0771b0c31d6d384
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-29 15:57:50 -0700

    use price change badge

--- frontend/src/components/StockCard.jsx
+++ frontend/src/components/StockCard.jsx
@@ -1,11 +1,13 @@
+import { PriceChangeBadge } from "./PriceChangeBadge";
+
 export const StockCard = ({ ticker, name, price, changePercent }) => {
   return (
     <div className="border=gray-200 flex items-center rounded-lg border bg-white p-2 shadow dark:border-gray-700 dark:bg-gray-800">
-      <div className="mr-2 h-8 w-8 rounded-lg bg-red-100"></div>
-      <div className="flex flex-col">
+      <div className="mr-2 flex flex-col">
         <h5 className="text-sm font-bold dark:text-white">{ticker}</h5>
         <div className="text-sm text-gray-500 dark:text-slate-400">{price}</div>
       </div>
+      <PriceChangeBadge percent={changePercent} />
     </div>
   );
 };
