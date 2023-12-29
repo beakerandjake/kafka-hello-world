9e431fdcb9bfdaaa35f0fbc63ab61dcae4cb789c
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-29 11:16:47 -0700

    add favicons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="min-h-full">
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+            Kafka Hello World 📈
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
