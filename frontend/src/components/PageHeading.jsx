a64a15d3b69a58adf6673b6dc2d80d79de78fedb
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-30 08:46:56 -0700

    change page heading

--- frontend/src/components/PageHeading.jsx
+++ frontend/src/components/PageHeading.jsx
@@ -7,8 +7,8 @@ export const PageHeading = () => (
       <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
         Kafka Hello World <span className="pl-1">📈</span>
       </h1>
-      <h2 className="text-md mt-2 hidden  text-gray-500 md:block dark:text-slate-400">
-        Real time (fake) stock prices with Kafka, Postgres, Fastify and React
+      <h2 className="text-sm mt-2 hidden  text-gray-500 md:block dark:text-slate-400">
+        Real time (fake) stock prices with Kafka
       </h2>
     </div>
   </header>
