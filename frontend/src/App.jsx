7befcfcb4edf457d2123d0c7bfce492742e21ba6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-29 17:26:11 -0700

    add click event

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,44 +3,28 @@ import { StockCard } from "./components/StockCard";
 
 const stocks = [
   {
+    id: 1,
     ticker: "FUN",
     price: 666.69,
     changePercent: -1.32,
   },
   {
+    id: 2,
     ticker: "YUM",
     price: 212.48,
     changePercent: 6.69,
   },
   {
-    ticker: "FUN",
-    price: 666.69,
-    changePercent: -1.32,
+    id: 3,
+    ticker: "CAT",
+    price: 420.69,
+    changePercent: 4.38,
   },
   {
-    ticker: "YUM",
-    price: 212.48,
-    changePercent: 6.69,
-  },
-  {
-    ticker: "FUN",
-    price: 666.69,
-    changePercent: -1.32,
-  },
-  {
-    ticker: "YUM",
-    price: 212.48,
-    changePercent: 6.69,
-  },
-  {
-    ticker: "FUN",
-    price: 666.69,
-    changePercent: -1.32,
-  },
-  {
-    ticker: "YUM",
-    price: 212.48,
-    changePercent: 6.69,
+    id: 4,
+    ticker: "DOG",
+    price: 13.98,
+    changePercent: -2.39,
   },
 ];
 
@@ -50,9 +34,13 @@ function App() {
       <PageHeading />
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <div className="flex w-full items-center gap-3 overflow-hidden overflow-x-auto pb-6">
+          <div className="flex w-full items-center gap-3  overflow-hidden overflow-x-auto pb-6">
             {stocks.map((stock) => (
-              <StockCard key={stock.ticker} {...stock} />
+              <StockCard
+                key={stock.ticker}
+                {...stock}
+                onClick={() => console.log("clicked on a stock", stock.ticker)}
+              />
             ))}
           </div>
         </div>
--- frontend/src/components/StockCard.jsx
+++ frontend/src/components/StockCard.jsx
@@ -1,8 +1,11 @@
 import { PriceChangeBadge } from "./PriceChangeBadge";
 
-export const StockCard = ({ ticker, name, price, changePercent }) => {
+export const StockCard = ({ ticker, price, changePercent, onClick }) => {
   return (
-    <div className="border=gray-200 flex items-center rounded-lg border bg-white p-2 shadow dark:border-gray-700 dark:bg-gray-800">
+    <div
+      className="flex cursor-pointer items-center rounded-lg border border-slate-300 bg-white p-2 shadow hover:bg-slate-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-slate-700"
+      onClick={onClick}
+    >
       <div className="mr-2 flex flex-col">
         <h5 className="text-sm font-bold dark:text-white">{ticker}</h5>
         <div className="text-sm text-gray-500 dark:text-slate-400">{price}</div>
