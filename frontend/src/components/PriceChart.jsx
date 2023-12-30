103ae1486b0a9cff0ae8a89c9b4efaacd598fa2c
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-30 13:35:15 -0700

    wip price chart

---
+++ frontend/src/components/PriceChart.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Chart from "react-apexcharts";
+
+const generateData = () => {
+  const data = [];
+  let price = 43.29;
+  const startDate = new Date(2022, 4, 1);
+  for (let i = 0; i < 390; i++) {
+    const positive = Math.random() > 0.5;
+    const amount = Math.random() * 10 * (positive ? 1 : -1);
+    price = Math.max(0, price + amount);
+    data.push({
+      x: new Date(startDate.getTime() + (i + 1) * 60000).getTime(),
+      y: price,
+    });
+  }
+  console.log(data);
+  return data;
+};
+
+export const PriceChart = () => {
+  const [options, setOptions] = useState({
+    chart: {
+      animations: {
+        enabled: false
+      },
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    stroke:{
+      width: 1.5
+    },
+    xaxis: {
+      type: "datetime",
+      labels: {
+        // formatter: (value, timestamp) => {
+        //   return "cool guy!";
+        // },
+        dateTimeFormatter: {
+          year: "yyyy",
+          month: "MMM 'yy",
+          day: "dd MMM",
+          hour: "HH:mm",
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: (value) => value,
+      },
+    },
+    stoke: {
+      width: 1,
+    },
+  });
+  const [series, setSeries] = useState([{ name: "test", data: generateData() }]);
+  return (
+    <div>
+      <Chart options={options} series={series} type="line" />
+    </div>
+  );
+};
