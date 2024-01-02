d6ccc3bc32dd15fcfd12215e57691622779dba8a
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-02 11:57:57 -0700

    add selected effect

--- frontend/src/components/StockCard.jsx
+++ frontend/src/components/StockCard.jsx
@@ -1,10 +1,22 @@
+import clsx from "clsx";
 import { PriceChangeBadge } from "./PriceChangeBadge";
 
-export const StockCard = ({ ticker, price, changePercent, onClick }) => {
+export const StockCard = ({ ticker, price, changePercent, isSelected, onClick }) => {
   return (
     <div
-      className="flex cursor-pointer items-center rounded-lg border border-slate-300 bg-white p-2 shadow hover:bg-slate-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-slate-700"
-      onClick={onClick}
+      className={clsx(
+        "m-1 flex cursor-pointer items-center rounded-lg border border-slate-300 bg-white p-2 shadow dark:border-gray-700 dark:bg-gray-800 ",
+        isSelected
+          ? "outline outline-2 outline-blue-500 dark:outline-slate-400"
+          : "hover:bg-slate-100 dark:hover:bg-slate-700",
+      )}
+      onClick={(e) => {
+        if (isSelected) {
+          e.stopPropagation();
+          return;
+        }
+        onClick();
+      }}
     >
       <div className="mr-2 flex flex-col">
         <h5 className="text-sm font-bold dark:text-white">{ticker}</h5>
