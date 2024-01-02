5c168997a48b649912a1b1187b9bfee6ef051923
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-02 13:01:06 -0700

    add more info to get all stocks endpoint

--- api/src/routes/getAllStocks.js
+++ api/src/routes/getAllStocks.js
@@ -3,9 +3,21 @@
  */
 const routes = async (fastify) => {
   fastify.get("/stocks", async () => {
-    const { rows } = await fastify.pg.query(
-      "SELECT id, ticker, full_name FROM stocks"
-    );
+    const { rows } = await fastify.pg.query(`
+      SELECT 
+        id,
+        ticker, 
+        full_name,
+        price as open_price, 
+        (
+          SELECT pc.price
+          FROM price_changes as pc
+          WHERE pc.ticker = s.ticker
+          ORDER BY pc.event_date DESC
+          LIMIT 1
+        ) as latest_price
+      FROM stocks as s
+    `);
     return rows;
   });
 };
