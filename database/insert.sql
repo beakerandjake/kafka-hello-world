a3e14a361ba7bd4546e97b952076fc7cab82ca35
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-02 16:41:39 -0700

    huge volatility test

--- database/insert.sql
+++ database/insert.sql
@@ -4,4 +4,4 @@ VALUES
     ('DOG', 'Good Dog Corporation', 144.66, 0.05),
     ('COOL', 'One Cool Company, Inc.', 55.95, 0.02),
     ('FUN', 'Fun Time Corp.', 98.95, 0.02),
-    ('ICE', 'Snow Cold Drinks Company Limited', 325.95, 0.02);
+    ('ICE', 'Snow Cold Drinks Company Limited', 325.95, 0.67);
