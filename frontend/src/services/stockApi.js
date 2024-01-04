7e7b9b544a470f9446a5f2b8958c987016c9ca65
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-04 15:50:59 -0700

    add helper method

--- frontend/src/services/stockApi.js
+++ frontend/src/services/stockApi.js
@@ -4,9 +4,20 @@ const API_ENDPOINT = import.meta.env.PROD ? "/api" : "http://localhost:3000";
 /**
  * Returns the latest price data for the stock.
  */
-export const getPriceData = async (ticker) => {
+export const getPrice = async (ticker) => {
   const response = await fetch(`${API_ENDPOINT}/stocks/${ticker}/price`);
   return await response.json();
+};
+
+/**
+ * Returns the latest prices of the stocks.
+ */
+export const getPrices = async (tickers) => {
+  const prices = await Promise.all(tickers.map((ticker) => getPrice(ticker)));
+  return prices.reduce((acc, { ticker, open, latest }) => {
+    acc[ticker] = { open, latest };
+    return acc;
+  }, {});
 };
 
 /**
