3b6d7574c27a9880e397651d3d9cfe6fff36869f
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-05 12:09:56 -0700

    parse date on realtime price

--- frontend/src/hooks/useRealtimePrices.js
+++ frontend/src/hooks/useRealtimePrices.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { API_ENDPOINT } from "../services/stockApi";
+import { parseISO } from "date-fns";
 
 /**
  * Hook which return a continual stream of latest price changes as they happen.
@@ -10,8 +11,9 @@ export const useRealtimePrices = () => {
     const sse = new EventSource(`${API_ENDPOINT}/stocks/realtime`);
     // update latest prices whenever server pushes new data.
     sse.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      setLatest({ ticker: data.ticker, price: data.price });
+      const parsed = JSON.parse(event.data);
+      parsed.date = parseISO(parsed.date);
+      setLatest(parsed);
     };
     return () => {
       sse.close();
