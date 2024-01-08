1814a6fb598da78bdcf93b85586ba2894dbb4824
beakerandjake <beakerandjake@users.noreply.github.com>
2024-01-07 23:22:56 -0700

    formatting

--- consumer-aggregate/src/consumer.py
+++ consumer-aggregate/src/consumer.py
@@ -20,13 +20,11 @@ consumer.subscribe([os.environ["CONSUMER_TOPIC"]])
 try:
     while True:
         message = consumer.poll(timeout=1.0)
-
         if message is None:
             continue
         if message.error():
             print(f"error consuming message: {message.error()}")
             continue
-
         value = json.loads(message.value().decode("utf-8"))
         print(f"consumed message: {value}")
         aggregate_price_change(value)
